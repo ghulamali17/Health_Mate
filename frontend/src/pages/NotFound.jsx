97a121101659f426f215e869dde7a429f15de7b4
ghulamali17 <ghulamali47092@gmail.com>
2025-10-19 12:10:03 +0500

    Update API endpoints to use new production URL in authentication and user components

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -18,7 +18,7 @@ function NotFound() {
         const token = localStorage.getItem("pos-token");
         if (!token) return;
 
-        const response = await axios.get("https://health-mate-dcv3.vercel.app/api/users/current", {
+        const response = await axios.get("https://health-mate-3x6x.vercel.app//api/users/current", {
           headers: { Authorization: `Bearer ${token}` },
         });
         setUser(response.data);
